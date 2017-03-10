4bd9515e46b2b87209a0a873336ef35f2cb1971a
rrueckert <rrueckert@avameo.de>
2017-03-10 16:54:32 +0100

    added some style to happyNumbers

--- js/happyNumbers.js
+++ js/happyNumbers.js
@@ -1,42 +1,59 @@
 (function ($) {
-	$(function() {
-        var $happyNumbers = $('#happyNumbers');
-        var arr = [];
-        for(var i = 1; i<=100; i++){
-            var res = getHappyNumber(i, i, []);
-            if(res > 0){
-                arr.push(res);
+    $(function () {
+
+        $('#output').click(function(e){
+            e.preventDefault();
+            getHappyNumbersComplete(Number($('#from').val()),Number($('#to').val()));
+        });
+        function getHappyNumbersComplete(start, end) {
+            var $happyNumbers = $('#happyNumbers');
+            var arr = [];
+            for (var i = start; i <= end; i++) {
+                var res = getHappyNumber(i, i, []);
+                if (res > 0) {
+                    arr.push(res);
+                }
             }
-        }
-        $happyNumbers.html(arr.join());
-	});	
 
-    function getHappyNumber(num, init, tmpArray){
-        tmpArray.push(num);
-        var arr = [];
-        var result = 0;
-        while(num > 0){
-            result += (num % 10)*(num % 10)
-            num = parseInt(num / 10);
+            $happyNumbers.html(buildHtml(arr));
         }
 
-        if(result == 1){
-            return init;
-        }else if(existInArray(result,tmpArray)){
-            return -1;
-        }else{
-            return getHappyNumber(result, init, tmpArray);
-            
-        }
-        
-    }
-
-    function existInArray(num, tmpArray){
-        for(var i = 0; i < tmpArray.length; i++){
-            if(num == tmpArray[i]){
-                return true;
+        function getHappyNumber(num, init, tmpArray) {
+            tmpArray.push(num);
+            var arr = [];
+            var result = 0;
+            while (num > 0) {
+                result += (num % 10) * (num % 10)
+                num = parseInt(num / 10);
             }
+
+            if (result == 1) {
+                return init;
+            } else if (existInArray(result, tmpArray)) {
+                return -1;
+            } else {
+                return getHappyNumber(result, init, tmpArray);
+
+            }
+
         }
-        return false;
-    }
+
+        function existInArray(num, tmpArray) {
+            for (var i = 0; i < tmpArray.length; i++) {
+                if (num == tmpArray[i]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function buildHtml(arr) {
+            var result = ""
+            for (var i = 0; i < arr.length; i++) {
+                result += "<div class='col-md-1 h-number' ><span>" + arr[i] + "</span></div>";
+            }
+            result += "";
+            return result;
+        }
+    });
 })(jQuery);
